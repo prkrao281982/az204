4daaa883cd5b98d75ed0d40745d6d3dfbb0114a7
Raj Panyam <rajkumar@eretailtech.com>
2022-08-22 14:43:18 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace az204.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
